e5f47b4c4a8f481ac92d304fdfb1404befcfaf61
Champ <varis_champ@hotmail.com>
2022-07-29 03:20:36 +0700

    finish making it happen

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
-import { CommandInteraction, GuildMember } from 'discord.js'
-import { isBlacklister, isAdmin } from './core/db/functions'
-import { isGuildOwner, isBotOwner, hasKickPerms } from './helpers/discord'
+import { CommandInteraction, GuildMember } from 'discord.js';
+import dotenv from 'dotenv';
+
+import { isAdmin, isBlacklister } from './core/db/functions';
+import { hasKickPerms, isBotOwner, isGuildOwner } from './helpers/discord';
+
+dotenv.config({ path: __dirname + '/../.env' });
 
 export const config: LunaBotConfig = {
   deeplKey: process.env.DEEPL_KEY,
@@ -18,23 +22,23 @@ export const config: LunaBotConfig = {
   twitcastingId: process.env.TWITCASTING_CLIENT_ID,
   twitcastingSecret: process.env.TWITCASTING_CLIENT_SECRET,
   holodexKey: process.env.HOLODEX_API_KEY,
-}
+};
 
 export interface PermLevel {
-  level: number
-  name: string
-  check: (x: CommandInteraction | GuildMember) => boolean | Promise<boolean>
+  level: number;
+  name: string;
+  check: (x: CommandInteraction | GuildMember) => boolean | Promise<boolean>;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 interface LunaBotConfig {
-  deeplKey?: string
-  ownerId: string
-  permLevels: PermLevel[]
-  prefix: string
-  token?: string
-  twitcastingId?: string
-  twitcastingSecret?: string
-  holodexKey?: string
+  deeplKey?: string;
+  ownerId: string;
+  permLevels: PermLevel[];
+  prefix: string;
+  token?: string;
+  twitcastingId?: string;
+  twitcastingSecret?: string;
+  holodexKey?: string;
 }
--- src/helpers/discord/loaders.ts
+++ src/helpers/discord/loaders.ts
@@ -1,89 +1,87 @@
-import { readdirSync } from 'fs'
-import {
-  ContextMenuCommandBuilder,
-  SlashCommandBuilder,
-  SlashCommandSubcommandsOnlyBuilder,
-} from '@discordjs/builders'
-import { log } from '../../helpers'
-import { CommandInteraction } from 'discord.js'
-import { Map as ImmutableMap } from 'immutable'
-import path from 'path'
-import { isNotNil } from '../language'
+import { ContextMenuCommandBuilder, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+import { readdirSync } from 'fs';
+import { Map as ImmutableMap } from 'immutable';
+import path from 'path';
+
+import { config } from '../../config';
+import { log } from '../../helpers';
+import { isNotNil } from '../language';
 
 export function loadCommand(cmdFile: string): Command {
-  return loadModule('commands', cmdFile)
+  return loadModule('commands', cmdFile);
 }
 
 export function loadEvent(evtFile: string): BotEvent {
-  return loadModule('events', evtFile)
+  return loadModule('events', evtFile);
 }
 
 export function loadAllCommands(): ImmutableMap<CommandName, Command> {
-  return loadAll('commands')
+  return loadAll('commands');
 }
 
 export function loadAllEvents(): ImmutableMap<EventName, BotEvent> {
-  return loadAll('events')
+  return loadAll('events');
 }
 
-export type BotEvent = (data?: unknown) => void
+export type BotEvent = (data?: unknown) => void;
 
 export interface Command {
   config: {
-    permLevel: number
-  }
+    permLevel: number;
+  };
   help: {
-    category: string
-    description: string
+    category: string;
+    description: string;
     // usage: string
-  }
+  };
   slash:
     | SlashCommandBuilder
     | SlashCommandSubcommandsOnlyBuilder
     | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
-    | ContextMenuCommandBuilder
-  callback: (intr: CommandInteraction) => void | Promise<void>
+    | ContextMenuCommandBuilder;
+  callback: (intr: CommandInteraction) => void | Promise<void>;
 }
 
 //// PRIVATE //////////////////////////////////////////////////////////////////
 
-type Module = Command & BotEvent
+type Module = Command & BotEvent;
 
-type EventName = string
+type EventName = string;
 
-type CommandName = string
+type CommandName = string;
 
 function loadModule(dir: string, moduleFile: string): Module {
-  const exportName = path.basename(moduleFile, '.js')
-  const modulePath = `../../core/${dir}/${moduleFile}`
-  const moduleObj = require(modulePath)[exportName] as Module
+  const exportName = path.basename(moduleFile, '.js');
+  const modulePath = `../../core/${dir}/${moduleFile}`;
+  const moduleObj = require(modulePath)[exportName] as Module;
 
-  log(`Loaded ${dir}/${moduleFile}`)
+  log(`Loaded ${dir}/${moduleFile}`);
 
-  return moduleObj
+  return moduleObj;
 }
 
 function loadAll(type: string): ImmutableMap<string, Module> {
-  const files = readdirSync(resolveRelativePath(`../../core/${type}`))
+  const files = readdirSync(resolveRelativePath(`../../core/${type}`));
   const modules_ = files
     .filter(isNotSourceMapFile)
     .map((f: string) => [path.basename(f, '.js'), loadModule(type, f)])
-    .filter(isNotNil)
-  const modules = filterUndefinedModules(ImmutableMap(modules_ as [string, Module][]))
+    .filter(isNotNil);
+  const modules = filterUndefinedModules(ImmutableMap(modules_ as [string, Module][]));
 
-  log(`Loaded ${modules.size} ${type}.`)
+  log(`Loaded ${modules.size} ${type}.`);
 
-  return modules as ImmutableMap<string, Module>
+  return modules as ImmutableMap<string, Module>;
 }
 
 function isNotSourceMapFile(file: string) {
-  return !file.endsWith('.map')
+  return !file.endsWith('.map');
 }
 
 function resolveRelativePath(target: string) {
-  return path.resolve(__dirname, target)
+  return path.resolve(__dirname, target);
 }
 
 function filterUndefinedModules(m: ImmutableMap<string, Module>): ImmutableMap<string, Module> {
-  return m.filter((module) => module !== undefined)
+  return m.filter((module) => module !== undefined);
 }
