60286c58e13f5b5f2dd7eace6ed5c70690d25915
Champ <varis_champ@hotmail.com>
2022-08-03 00:34:38 +0700

    save

--- src/core/db/functions/botData.ts
+++ src/core/db/functions/botData.ts
@@ -1,64 +1,66 @@
-import { DocumentType } from '@typegoose/typegoose'
-import Enmap from 'enmap'
-import { UpdateQuery } from 'mongoose'
-import { setKey, filter } from '../../../helpers/immutableES6MapFunctions'
-import { VideoId } from '../../../modules/holodex/frames'
-import { BotData, BotDataDb } from '../models'
-import { RelayedComment } from '../models/RelayedComment'
+import { DocumentType } from '@typegoose/typegoose';
+import Enmap from 'enmap';
+import { UpdateQuery } from 'mongoose';
 
-const _id = '000000000022'
+import { filter, setKey } from '../../../helpers/immutableES6MapFunctions';
+import { VideoId } from '../../../modules/holodex/frames';
+import { BotData, BotDataDb } from '../models';
+import { RelayedComment } from '../models/RelayedComment';
 
-export const botDataEnmap = new Enmap({ name: 'botData' })
+const _id = '000000000022';
+
+export const botDataEnmap = new Enmap({ name: 'botData' });
 
 export function addNotifiedLive(videoId: VideoId): void {
-  const currentList = botDataEnmap.ensure('notifiedYtLives', []) as VideoId[]
-  botDataEnmap.set('notifiedYtLives', [...currentList, videoId] as VideoId[])
+  const currentList = botDataEnmap.ensure('notifiedYtLives', []) as VideoId[];
+  botDataEnmap.set('notifiedYtLives', [...currentList, videoId] as VideoId[]);
 }
 
 export function getNotifiedLives(): VideoId[] {
-  return botDataEnmap.ensure('notifiedYtLives', []) as VideoId[]
+  return botDataEnmap.ensure('notifiedYtLives', []) as VideoId[];
 }
 
 export function addNotifiedCommunityPost(url: string): void {
-  const currentList = botDataEnmap.ensure('notifiedCommunityPosts', [])
-  botDataEnmap.set('notifiedCommunityPosts', [...currentList, url])
+  const currentList = botDataEnmap.ensure('notifiedCommunityPosts', []);
+  botDataEnmap.set('notifiedCommunityPosts', [...currentList, url]);
 }
 
 export function getNotifiedCommunityPosts(): string[] {
-  return botDataEnmap.ensure('notifiedCommunityPosts', []) as string[]
+  return botDataEnmap.ensure('notifiedCommunityPosts', []) as string[];
 }
 
 export async function getBotData(): Promise<BotData> {
-  const query = [{ _id }, {}, { upsert: true, new: true }] as const
-  return BotDataDb.findOneAndUpdate(...query)
+  const query = [{ _id }, {}, { upsert: true, new: true }] as const;
+  return BotDataDb.findOneAndUpdate(...query);
 }
 
 export async function getRelayHistory(videoId?: VideoId): Promise<RelayedComment[] | undefined> {
-  const botData = await getBotData()
-  const hists = botData.relayHistory
-  return hists.get(videoId ?? '')
+  const botData = await getBotData();
+  const hists = botData.relayHistory;
+  return hists.get(videoId ?? '');
 }
 
 export async function addToBotRelayHistory(videoId: VideoId, cmt: RelayedComment): Promise<void> {
-  const history = (await getBotData()).relayHistory
-  const cmts = history.get(videoId) ?? []
-  const newHistory = setKey(videoId, [...cmts, cmt])(history)
-  updateBotData({ relayHistory: newHistory })
+  const history = (await getBotData()).relayHistory;
+  const cmts = history.get(videoId) ?? [];
+  const newHistory = setKey(videoId, [...cmts, cmt])(history);
+  updateBotData({ relayHistory: newHistory });
 }
 
 export async function clearOldBotData() {
-  const history = (await getBotData()).relayHistory
-  console.log(`history len is ${history.size}`)
-  const newHistory = filter(history, (v, k) => v[0].absoluteTime > Date.now() - 86400000)
-  console.log(`new his len ${newHistory.size}`)
-  updateBotData({ relayHistory: newHistory })
+  const history = (await getBotData()).relayHistory;
+  console.log(`history len is ${history.size}`);
+  const newHistory = filter(history, (v, k) => v[0].absoluteTime > Date.now() - 86400000);
+  console.log(`new his len ${newHistory.size}`);
+  updateBotData({ relayHistory: newHistory });
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 async function updateBotData(update: NewData): Promise<void> {
-  const query = [{ _id }, update, { upsert: true, new: true }] as const
-  await BotDataDb.findOneAndUpdate(...query)
+  const query = [{ _id }, update, { upsert: true, new: true }] as const;
+  const doc = await BotDataDb.findOneAndUpdate(...query);
+  console.log('🚀 ~ file: botData.ts ~ line 62 ~ updateBotData ~ doc', doc);
 }
 
-type NewData = UpdateQuery<DocumentType<BotData>>
+type NewData = UpdateQuery<DocumentType<BotData>>;
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ import { client } from './core/';
 Error.stackTraceLimit = Infinity;
 dotenv.config({ path: __dirname + '/../.env' });
 const MONGODB_URL = process.env.MONGODB_URL ?? 'mongodb://localhost/luna';
-console.log('🚀 ~ file: index.ts ~ line 13 ~ MONGODB_URL', MONGODB_URL);
 
 mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
