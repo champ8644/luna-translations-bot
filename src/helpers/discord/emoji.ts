c7ee580263a515d52dc2d2721f40c62a60966bb1
Champ <varis_champ@hotmail.com>
2022-08-22 22:10:54 +0700

    save

--- src/helpers/discord/emoji.ts
+++ src/helpers/discord/emoji.ts
@@ -9,11 +9,11 @@ export const emoji: Record<Name, EmojiCode> = {
   yt: '<:YouTube:832638929802493962>',
   peek: '<:LunaPeek:873613928867975248>',
   niji: '<:nijisanji:893782660156112986>',
-  pixela: ':PixelaLogo:1011286210485956618',
-  isekai: ':IsekaiLogo:1011286194312724581',
-  legends: ':LegendLogo:1011286201120063489',
-  polygon: ':PolygonIcon:1011290108072304771',
-  ataru: ':AtaruKun:1011290096244371626',
+  pixela: '<:PixelaLogo:1011286210485956618>',
+  isekai: '<:IsekaiLogo:1011286194312724581>',
+  legends: '<:LegendLogo:1011286201120063489>',
+  polygon: '<:PolygonIcon:1011290108072304771>',
+  ataru: '<:AtaruKun:1011290096244371626>',
 } as const;
 
 ///////////////////////////////////////////////////////////////////////////////
